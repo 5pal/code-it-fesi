57406e32cbfea767843f404bf94839cd72cb6ef3
Opal <how2logout@gmail.com>
2024-12-12 18:18:12 +0900

    feat: practice tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <div className="w-full h-screen bg-neutral-200 flex justify-center items-center">
-      <div className="w-10 h-10 bg-red-400"></div>
+      <div className="w-48 h-48 bg-red-400 text-center">
+        <p className="text-blue-700"></p>
+        <p className="text-red-700"></p>
+        <p className="text-yellow-600"></p>
+      </div>
     </div>
   );
 }
